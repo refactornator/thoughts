b1f176935d057f9859bfe825e470aa8c18e132a7
wlindner <wlindner522@gmail.com>
2016-12-10 16:39:09 -0800

    Initial commit.

---
+++ src/main/java/com/wlindner/ThoughtsApplication.java
@@ -0,0 +1,12 @@
+package com.wlindner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtsApplication.class, args);
+	}
+}
