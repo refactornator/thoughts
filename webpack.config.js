b48102c6809e2e97a5cb343ddffdd87f04f5391c
William Lindner <wlindner522@gmail.com>
2017-01-15 16:27:03 -0800

    Support the spread operator

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                 loader: 'babel',
                 query: {
                     cacheDirectory: true,
-                    presets: ['es2015', 'react']
+                    presets: ['es2015', 'react', 'stage-0']
                 }
             }
         ]
