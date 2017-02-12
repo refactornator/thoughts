6ee539dbc2b95462a749038cceec0cc7de0421b8
William Lindner <wlindner522@gmail.com>
2017-02-11 18:00:44 -0800

    Upgrade Webpack to v2

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,22 @@ module.exports = {
     entry: ['whatwg-fetch', './src/main/js/start.js'],
     devtool: 'sourcemaps',
     cache: true,
-    debug: true,
     output: {
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react', 'stage-0']
-                }
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['es2015', 'react', 'stage-0']
+                    }
+                }]
             }
         ]
     }
