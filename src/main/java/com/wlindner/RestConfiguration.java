1237e3c3c2c0512d1d3432e1f9fe406c98347bd2
William Lindner <wlindner522@gmail.com>
2017-05-20 20:50:38 -0700

    Upgrade the Spring Boot dependency version

--- src/main/java/com/wlindner/RestConfiguration.java
+++ src/main/java/com/wlindner/RestConfiguration.java
@@ -1,14 +1,14 @@
 package com.wlindner;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class RestConfiguration extends RepositoryRestMvcConfiguration {
- 
+@Component
+public class RestConfiguration extends RepositoryRestConfigurerAdapter {
+
     @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Thought.class);
     }
 }
